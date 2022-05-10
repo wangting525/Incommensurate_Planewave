98b5bda3eafd16cfb5c7d6f0dcf497cffe5f08f2
wangting1111 <441393163@qq.com>
2022-05-10 23:32:20 +0800

    2d_DoS

---
+++ example/2D_H_DoS.jl
@@ -0,0 +1,58 @@
+using Plots
+using LaTeXStrings
+
+include("../src/struct.jl")
+include("../src/2Dincommensurate.jl")
+
+# set atom parameters 2d
+X1 = [0.0;0.0]
+X2 = [0.0;0.0]
+s=1  
+θ=π/10     # angle of rotation
+L=2
+A1=[1 0; 0 1]  
+R1 =L*A1     #   sheet 1
+A2=[cos(θ) cos(θ+π/2); sin(θ) sin(θ+π/2)] 
+R2 =L*A2   # sheet 2 is rotated by θ=π/10
+v(x,y)=s*((cos((π/L)*x))^2)*((cos((π/L)*y))^2)
+v1(x,y)=v((A1*[x;y])[1],(A1*[x;y])[2])
+v2(x,y)=v((A2*[x;y])[1],(A2*[x;y])[2])
+#v1(x,y)=s1*(cos(π*(A1*[x;y])[1]))^2*(cos(π*(A1*[x;y])[2]))^2  
+#v2(x,y)=s2*(cos(π*(A2*[x;y])[1]))^2*(cos(π*(A2*[x;y])[2]))^2 
+B1,B2=reciprocal(R1,R2)
+atoms= TwoLayerIn2D(R1=R1, R2=R2,B1=B1,B2=B2,v1=v1, v2=v2,θ=θ, X1=X1, X2=X2)
+
+# area of the reciprocal unit cell
+RS1 = sqrt( norm(B1[:,1])^2 * norm(B1[:,2])^2 - dot(B1[:,1], B1[:,2])^2 ) 
+RS2 = sqrt( norm(B2[:,1])^2 * norm(B2[:,2])^2 - dot(B2[:,1], B2[:,2])^2 )
+
+Coe=RS1*RS2*π/4
+
+# set the model parameters
+EcL = 20.0
+EcW = 5.0
+neigs=10
+nfx =4* floor( Int, (2.0 * EcL) / norm(B1[:,1]) )    # 4* Gmax11
+nfy= 4* floor( Int, (2.0 * EcL) / norm(B1[:,2]) )    # 4* Gmax12
+γ=0.01
+kpts=[0.0 0.0;0.0 0.0]
+model = pwIncommensurate2D(EcL=EcL, EcW=EcW, kpts=kpts, n_fftwx=nfx, n_fftwy=nfy, n_eigs=neigs, γ=γ)    
+
+# test function
+g_fermi_beta01mu10(x) = x*(1+exp((x-10)*0.1))^(-1)
+g_fermi_beta1mu10(x) = x*(1+exp((x-10)*1))^(-1)
+g_fermi_beta10mu10(x) = x*(1+exp((x-10)*10))^(-1)
+g(x)=g_fermi_beta1mu10(x)
+
+# hamiltonian
+H, G, Gmn, R, Gmax11,Gmax12,Gmax21,Gmax22=hamiltonian2d(atoms, model)
+
+λ = zeros(Float64, size(H,1));
+D, Ψ = eigen(Array(H));
+
+ # DoS for test function with β=1,μ=10 
+ d=zeros(Float64,length(D))
+ for i=1:length(D)
+	 d[i]=g(D[i])
+ end 
+ dos=Coe*(1/EcL^2)*sum(d)
